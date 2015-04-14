df10df3ce712270bff67288e7ec32a285bfd15a3
rhard32 <rhard32@gmail.com>
2015-04-14 13:56:11 -0400

    Created Fail test

---
+++ TddLab.Test/TddLabTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TddLab;
+
+namespace TddLab.Test
+{
+    public class TddLabTest
+    {
+        [Fact]
+        public void StreetNamePropertyReturnsNotVoid()
+
+    {
+        var testStreetName = new streetName();
+
+        Assert.False(testStreetName == null);
+
+
+    }
+    }
+}
--- TddLab/streetName.cs
+++ TddLab/streetName.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TddLab.Test
+namespace TddLab
 {
-    public class Class1
+    public class streetName
     {
     }
 }
