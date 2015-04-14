b40c881f760576187dd95fc7bb230b677589e5ac
MarkJensen55 <markjensen@tds.net>
2015-04-14 15:37:09 -0400

    Updated getconsonants to handle numbers

--- TddLab/streetName.cs
+++ TddLab/streetName.cs
@@ -11,12 +11,13 @@ namespace TddLab
         public List<char> GetConsanants(String input)
         {
             var constList = new List<char>() ;
-            var vowels = new List<char>() { 
-            'a','e','i','o','u','A','E','I','O','U',' '}; 
+            var consonants = new List<char>() { 
+            'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','v',
+            'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','V'}; 
 
             for (int i = 0; i < input.Length; i++)
 			{
-                if (!(vowels.Contains(input[i])))
+                if (consonants.Contains(input[i]))
                 {
                     constList.Add(input[i]);
                 }
